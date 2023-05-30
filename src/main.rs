fefc8c315e4b17bf1467fb5fd6beef02cd78805d
etorresh <me@etorresh.com>
2023-05-29 19:42:37 -0600

    first commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+// Singly linked ist
+fn main() {
+    // let x = Node {
+    //     data: 5,
+    //     next: None,
+    // };
+    // let y = Node {
+    //     data: 10,
+    //     next: Some(Box::new(x)),
+    // };
+    // let z = Node {
+    //     data: 15,
+    //     next: Some(Box::new(y)),
+    // };
+
+    // let head = z;
+    // if head.next.unwrap().next.unwrap().data == 5 {
+    //     println!("I can see the last value")
+    // }
+    let x: LinkedList<i32> = LinkedList {
+        head: None,
+        counter: 0,
+    };
+}
+
+struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>>,
+}
+
+struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
+    counter: usize,
+}
+
+impl<T> LinkedList<T> {
+    fn add_first(&mut self, data: T) {
+        let new_node = Box::new(Node {
+            data,
+            next: self.head.take(),
+        });
+
+        self.head = Some(new_node);
+    }
+
+    fn add_last(&mut self, data: T) {
+        let new_node = Box::new(Node { data, next: None });
+        let current;
+        while (current != None) {
+            current = &self.head;
+        }
+    }
+}
