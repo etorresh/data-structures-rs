a41e3e696b0189c5dfd2767bdea40f2a54340a67
etorresh <me@etorresh.com>
2023-06-01 00:02:42 -0600

    ugly chained and_then

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // Singly linked ist
 fn main() {
     let mut x: LinkedList<i32> = LinkedList::new();
-    x.remove_first();
+    x.add_first(5);
+    x.add_first(10);
+    x.remove_last();
+    println!("{}", x.head.unwrap().data);
 }
 
 struct Node<T> {
@@ -58,7 +61,7 @@ impl<T> LinkedList<T> {
         // Count and cut: traverse once and count the values. Traverse again and stop at count - 1
         // Best option: use current and previous, but how the fuck do we handle two mutable references?
 
-        /* */
+        /* COUNT AND CUT
         if self.head.is_none() {
             return;
         }
@@ -82,6 +85,26 @@ impl<T> LinkedList<T> {
         }
         *current = current.take().and_then(|node| node.next);
         self.counter -= 1;
+        */
+
+        /* LOOK AHEAD */
+        if self.head.is_none() {
+            return;
+        }
+        let mut current = &mut self.head;
+        loop {
+            if current
+                .as_mut()
+                .and_then(|node| node.next.as_mut())
+                .and_then(|next_node| next_node.next.as_mut())
+                .is_some()
+            {
+                *current = current.take().and_then(|node| node.next);
+            } else {
+                self.counter -= 1;
+                break;
+            }
+        }
     }
     fn remove() {}
     fn find() {}
