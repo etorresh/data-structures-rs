50f22d51b3b81dde8d792fc397ca3b957bd99dad
Garry Host <me@etorresh.com>
2023-08-02 19:36:39 -0600

    template

---
+++ src/hash.rs
@@ -0,0 +1,7 @@
+use crate::singly_linked_list::LinkedList;
+
+pub struct HashMap<T> {
+    hash_array: Vec<LinkedList<T>>,
+}
+
+impl<T> HashMap<T> {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod doubly_linked_list;
+mod hash;
 mod singly_linked_list;
 
 pub use doubly_linked_list::DoublyLinkedList;
+pub use hash::HashMap;
 pub use singly_linked_list::LinkedList;
