462be34b73b866c3de5ea52693bd8992211b0b71
Garry Host <me@etorresh.com>
2023-08-20 22:27:24 -0600

    added starting point for hashmap

--- src/hash_chaining.rs
+++ src/hash_chaining.rs
@@ -1,35 +1,72 @@
 // a hash implementation that uses chaining to avoid collisions.
 use crate::singly_linked_list::LinkedList;
-use std::collections::hash_map::DefaultHasher;
-use std::collections::hash_map::RandomState;
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{self, Hash, Hasher},
+};
 
-pub struct HashMap<K, V> {
+const MAX_LOAD_FACTOR: f32 = 0.75;
+
+pub struct HashMap<K: Hash, V> {
     hash_array: Vec<LinkedList<(K, V)>>,
+    size: usize,
 }
 
-impl<K, V> HashMap<K, V> {
+impl<K: Hash, V> HashMap<K, V> {
     pub fn new() -> HashMap<K, V> {
         let hash_array = Vec::new();
-        HashMap { hash_array }
+        HashMap {
+            hash_array,
+            size: 0,
+        }
     }
     pub fn with_capacity(capacity: usize) -> HashMap<K, V> {
         let hash_array = (0..capacity).map(|_| LinkedList::new()).collect();
-        HashMap { hash_array }
+        HashMap {
+            hash_array,
+            size: 0,
+        }
     }
 
     // Returns old value if key was previously present
-    fn insert(&mut self, key: K, value: V) -> Option<V> {
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        if self.hash_array.len() == 0 {
+            *self = HashMap::with_capacity(2);
+        }
+
+        // Load factor
+        if ((self.size as f32 + 1.0) / self.hash_array.len() as f32) >= MAX_LOAD_FACTOR {
+            self.rehash();
+        }
+
+        let index = self.bucket_index(&key);
+
+        let bucket = &mut self.hash_array[index];
+
+        // Come back here after implementing iter_mut in LinkedList
+
+        self.size += 1;
         None
     }
 
-    fn get(&self, key: K) -> Option<V> {
+    pub fn get(&self, key: K) -> Option<V> {
         None
     }
 
     // Returns removed value
-    fn remove(&mut self, key: K) -> Option<V> {
+    pub fn remove(&mut self, key: K) -> Option<V> {
         None
     }
+
+    fn bucket_index(&self, key: &K) -> usize {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        let hash_value = hasher.finish();
+        let bucket_index = (hash_value as usize) % self.hash_array.len();
+        bucket_index
+    }
+
+    fn rehash(&mut self) {}
 }
 
 #[cfg(test)]
