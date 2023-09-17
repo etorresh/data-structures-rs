0f2e8cb81fa7216aab5c04cd34bec2e4fb2ed5ee
Garry Host <me@etorresh.com>
2023-09-16 22:17:30 -0600

    feat(tree_avl): Initial AVL tree setup

--- src/tree_avl.rs
+++ src/tree_avl.rs
@@ -1,1 +1,43 @@
-todo!();
+/*
+AVL Tree
+- Difference in height between left and right must always be <= 1.
+*/
+
+struct Node<T> {
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>,
+    data: T,
+}
+
+pub struct TreeAVL<T: Ord> {
+    root: Option<Box<Node<T>>>,
+}
+
+impl<T: Ord> TreeAVL<T> {
+    pub fn new() -> Self {
+        TreeAVL { root: None }
+    }
+
+    pub fn insert(&mut self, data: T) {
+        match &mut self.root {
+            Some(node) => {
+                Self::insert_recursive(node, &data);
+            }
+            None => {
+                self.root = Some(Box::new(Node {
+                    left: None,
+                    right: None,
+                    data,
+                }));
+            }
+        }
+    }
+
+    fn insert_recursive(node: &mut Node<T>, data: &T) {
+        todo!()
+    }
+
+    pub fn remove() {}
+
+    pub fn search() {}
+}
