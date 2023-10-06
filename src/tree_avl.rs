b3fa999bfe0f0a4a1ae830a01e9db4824addfbe1
Garry Host <me@etorresh.com>
2023-10-05 23:14:47 -0600

    Added initial rebalancing logic for AVL tree

--- src/tree_avl.rs
+++ src/tree_avl.rs
@@ -92,9 +92,15 @@ impl<T: Ord> TreeAVL<T> {
     }
 
     fn balance_factor(node: &Box<Node<T>>) -> isize {
-        let height_left = node.left.as_ref().map_or(0, Self::height);
-        let height_right = node.right.as_ref().map_or(0, Self::height);
-        let balance_factor: isize = (height_left - height_right).try_into().unwrap();
+        let height_left: isize = node
+            .left
+            .as_ref()
+            .map_or(-1, |node| node.height.try_into().unwrap());
+        let height_right: isize = node
+            .right
+            .as_ref()
+            .map_or(-1, |node| node.height.try_into().unwrap());
+        let balance_factor: isize = height_left - height_right;
         balance_factor
     }
 
@@ -105,11 +111,55 @@ impl<T: Ord> TreeAVL<T> {
         }
     }
 
-    fn rebalance(node: &mut Box<Node<T>>, balance_factor: isize) {}
+    /*
+    Balance factor is positive:
+        This indicates that the left subtree is taller than the right subtree.
+        If the balance factor is 1, then the left subtree is just one level deeper than the right subtree.
+        If the balance factor is greater than 1 then it's a sign the AVL property is violated, and the tree is too heavy on the left side.
+    Balance factor is negative:
+        This indicates that the right subtree is taller than the left subtree.
+        If the balance factor is -1, then the right subtree is one level deeper than the left subtree.
+        If the balance factor is less than -1, then it's a sign that the AVL property is violated, and the t ree is too heavy on the right side.
+    Balance factor is zero:
+     */
+    fn rebalance(node: &mut Box<Node<T>>, balance_factor: isize) {
+        // Too heavy on the left side
+        if balance_factor > 1 {
+            let left_child_balance_factor = Self::balance_factor(node.left.as_ref().unwrap());
+            if left_child_balance_factor >= 1 {
+                Self::rotate_rr(node);
+            } else if left_child_balance_factor <= -1 {
+                Self::rotate_rl();
+            } else {
+                panic!("reached impossible case child balance factor is between -1 and 1 non-inclusive");
+            }
+        }
+        // Too heavy on the right side
+        else if balance_factor < -1 {
+            let right_child_balance_factor = Self::balance_factor(node.right.as_ref().unwrap());
+            if right_child_balance_factor <= -1 {
+                Self::rotate_ll();
+            } else if right_child_balance_factor >= 1 {
+                Self::rotate_lr();
+            } else {
+                panic!("reached impossible case child balance factor is between -1 and 1 non-inclusive");
+            }
+        }
+    }
 
+    // Rotate left when the problem is on the right subtree.
+    // Rotate right when the problem is on the left subtree.
+
+    // Balance factor of the current node is < -1, and balance factor of the right child is <= -1.
     fn rotate_ll() {}
-    fn rotate_rr() {}
+
+    // Balance factor of the current node is > 1, and balance factor of the right child is <= -1.
+    fn rotate_rr(node: &mut Box<Node<T>>) {}
+
+    // Balance factor of the current node is < -1, and balance factor of the right child is >= 1.
     fn rotate_lr() {}
+
+    // Balance factor of the current node is > 1, and balance factor of the right child is >= 1.
     fn rotate_rl() {}
 
     pub fn remove() {}
